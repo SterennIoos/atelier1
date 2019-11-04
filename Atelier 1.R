df5ebd7df42e9a27d8ad98668a526cbadba39fa9
SterennIoos <55700862+SterennIoos@users.noreply.github.com>
2019-11-04 16:59:22 +0100

    Add files via upload

---
+++ Atelier 1.R
@@ -0,0 +1,15 @@
+2 + 3
+3*9
+12-48
+-12/3
+3,03-sqrt(2)
+x <-2
+y <-seq(from=0,to=10,by=1)
+y[2]
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles + 20
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(tailles)
+tailles.m <- tailles/100
+imc <- poids/tailles.m^2
+imc
